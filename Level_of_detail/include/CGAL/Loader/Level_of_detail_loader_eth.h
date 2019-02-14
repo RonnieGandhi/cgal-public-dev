5ebc44600a9a0c97e2d635663d17ab9a216ee525
Dmitry Anisimov <danston@ymail.com>
2019-02-14 17:05:10 +0100

    updated loader

--- Level_of_detail/include/CGAL/Loader/Level_of_detail_loader_eth.h
+++ Level_of_detail/include/CGAL/Loader/Level_of_detail_loader_eth.h
@@ -49,54 +49,60 @@ namespace CGAL {
 
 			void get_data(const std::string &filePath, Container &input) const override {
 
-            	std::ifstream loader(filePath.c_str(), std::ios_base::in);
+        std::ifstream loader(filePath.c_str(), std::ios_base::in);
             	
-            	// std::ofstream saver((filePath + ".new").c_str(), std::ios_base::out); saver.precision(20);
+        // std::ofstream saver((filePath + ".new").c_str(), std::ios_base::out); saver.precision(20);
 
-            	if (!loader) {
-                	std::cerr << std::endl << std::endl << "ERROR: Error loading file with LOD data!" << std::endl << std::endl;
-                	exit(EXIT_FAILURE);
-            	}
+        if (!loader) {
+          std::cerr << std::endl << std::endl << "ERROR: Error loading file with LOD data!" << std::endl << std::endl;
+          exit(EXIT_FAILURE);
+        }
 
 				Color_map colors; Label_map labels; Types_map types; Index_map indices;
 				set_default_properties(input, colors, labels, types, indices);
 
-            	std::string tmp; // , tmpimp;
-            	std::getline(loader, tmp);
-            	std::getline(loader, tmp);
-            	std::getline(loader, tmp);
-            	std::getline(loader, tmp);
-            	std::getline(loader, tmp);
-            	std::getline(loader, tmp);
-            	std::getline(loader, tmp);
-            	std::getline(loader, tmp);
+        std::string tmp;
+        std::getline(loader, tmp);
+        std::getline(loader, tmp);
+        std::getline(loader, tmp);
+        std::getline(loader, tmp);
+        std::getline(loader, tmp);
+        std::getline(loader, tmp);
+        std::getline(loader, tmp);
+        std::getline(loader, tmp);
 
-            	size_t num_points;
-            	loader >> tmp >> tmp >> num_points;
+        size_t num_points;
+        loader >> tmp >> tmp >> num_points;
 
-            	for (size_t i = 0; i < 13; ++i) std::getline(loader, tmp);
+        for (size_t i = 0; i < 13; ++i) 
+					std::getline(loader, tmp); // 20
 
 				FT x, y, z, nx, ny, nz;
-				int r, g, b, la;
+				int r, g, b, la, tr, nl;
 
-            	for (size_t i = 0; i < num_points; ++i) {
+				for (size_t i = 0; i < num_points; ++i) {
 
-            		loader >> x >> y >> z >> nx >> ny >> nz >> r >> g >> b >> tmp >> la;
+					loader >> x >> y >> z >> nx >> ny >> nz >> r >> g >> b >> tmp >> la;
 
-					// loader >> x >> y >> z >> nx >> ny >> nz >> r >> g >> b >> tmpimp >> la >> tmp;
-            		// saver << x << " " << y << " " << z << " " << nx << " " << ny << " " << nz << " " << r << " " << g << " " << b << " " << tmpimp << " " << la << std::endl;
+					// loader >> x >> y >> z >> tmp >> tmp >> tmp >> tmp >> tmp >> tmp >> tmp >> r >> g >> b >> tr >> la >> nx >> ny >> nz;
+
+					// nl = la;
+					// if (la == 3) nl = -1;
+					// if (la == 1) nl = 3; 
+
+					// saver << x << " " << y << " " << z << " " << nx << " " << ny << " " << nz << " " << r << " " << g << " " << b << " " << tr << " " << nl << std::endl;
 
 					Iterator it = input.insert(Point(x, y, z), Normal(nx, ny, nz));
 
-					 colors[*it] = {{static_cast<Type>(r), static_cast<Type>(g), static_cast<Type>(b)}};
-					 labels[*it] = la;
-					  types[*it] = -1; 
+					colors[*it] = {{static_cast<Type>(r), static_cast<Type>(g), static_cast<Type>(b)}};
+					labels[*it] = la;
+					types[*it] = -1; 
 					indices[*it] = -1;
-            	}
-            	loader.close();
+        }
+        loader.close();
             	
-            	// saver.close();
-            	// exit(0);
+        // saver.close();
+        // exit(0);
 			}
 
 		private:
